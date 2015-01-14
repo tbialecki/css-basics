93217296876b2571e57d45c3cd8a91c75a283c73
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2015-01-14 15:33:55 +0100

    css-basics

---
+++ app/js/custom.js
@@ -0,0 +1,9 @@
+$('pre').click(function (event) {
+    zoom.to({
+        element: this
+    });
+    event.preventDefault();
+});
+$.each($('code[data-trim=""]'),function(k,v){
+    $(v).html($.trim($(v).html()));
+});
